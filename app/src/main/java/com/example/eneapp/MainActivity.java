699acf9b806f2dbc7ca2b04c423da6d4d8c72573
mkmaq <65548640+mkmaq@users.noreply.github.com>
2022-05-19 16:24:00 +0900

    5/19 16:23

--- app/src/main/java/com/example/eneapp/MainActivity.java
+++ app/src/main/java/com/example/eneapp/MainActivity.java
@@ -35,13 +35,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        reference.child("key2").setValue("おはようございます", null);
-
-
-
-
-
 
 
     }
