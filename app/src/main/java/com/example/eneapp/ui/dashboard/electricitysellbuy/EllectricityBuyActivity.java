50b1829e3e07bbb082af226d6ab3e036cd633483
j3-miya <gongzeyouguig@gmaial.com>
2022-05-24 16:42:26 +0900

    5/24　4時のやつ

--- app/src/main/java/com/example/eneapp/ui/dashboard/electricitysellbuy/EllectricityBuyActivity.java
+++ app/src/main/java/com/example/eneapp/ui/dashboard/electricitysellbuy/EllectricityBuyActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.eneapp.MainActivity;
 import com.example.eneapp.R;
+import com.example.eneapp.ui.coin.result.PracticeActivity;
 
 public class EllectricityBuyActivity extends AppCompatActivity {
 
@@ -19,6 +23,20 @@ public class EllectricityBuyActivity extends AppCompatActivity {
         String text = intent.getStringExtra("buy2");
         TextView textView = (TextView)this.findViewById(R.id.textView16);
         textView.setText("取引電力:           "+text+"kWh");
+
+        Button sendButton21 =findViewById(R.id.button9);
+        // lambda式
+        Intent intent21 = new Intent(this, MainActivity.class);
+        sendButton21.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // クリック時の処理
+
+                startActivity(intent21);
+            }
+        });
+
+
+
 
     }
 }
