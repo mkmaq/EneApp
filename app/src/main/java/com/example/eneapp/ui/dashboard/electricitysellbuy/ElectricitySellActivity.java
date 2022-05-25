201dfccf648845c1d2b65fb47046dc48da5f1a1b
mkmaq <65548640+mkmaq@users.noreply.github.com>
2022-05-25 10:09:02 +0900

    5/24 masaki 10:08

--- app/src/main/java/com/example/eneapp/ui/dashboard/electricitysellbuy/ElectricitySellActivity.java
+++ app/src/main/java/com/example/eneapp/ui/dashboard/electricitysellbuy/ElectricitySellActivity.java
@@ -1,17 +1,31 @@
 package com.example.eneapp.ui.dashboard.electricitysellbuy;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.eneapp.MainActivity;
 import com.example.eneapp.R;
+import com.example.eneapp.databinding.FragmentSonotaBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import com.example.eneapp.databinding.ActivityEllectricityBuyBinding;
 
 public class ElectricitySellActivity extends AppCompatActivity {
+
+    private ActivityEllectricityBuyBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,34 @@ public class ElectricitySellActivity extends AppCompatActivity {
         String text = intent.getStringExtra("buynumber");
         TextView textView = (TextView)this.findViewById(R.id.textView16);
         textView.setText("取引電力：     "+text+"kWh");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.child("storage_battery").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                HashMap value = (HashMap) dataSnapshot.getValue();
+                Log.d(TAG, "Value is: " + value);
+
+                long souhatuden = (long) value.get("souhatuden");
+                long syouhi = (long) value.get("syouhi");
+                long yojo = (long) value.get("yojo");
+
+                TextView yojo1 = binding.yojosell1;
+                yojo1.setText(value.get("yojo") + " kWh");
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
 
 
--- app/src/main/java/com/example/eneapp/ui/dashboard/electricitysellbuy/EllectricityBuyActivity.java
+++ app/src/main/java/com/example/eneapp/ui/dashboard/electricitysellbuy/EllectricityBuyActivity.java
@@ -1,18 +1,32 @@
 package com.example.eneapp.ui.dashboard.electricitysellbuy;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.eneapp.MainActivity;
 import com.example.eneapp.R;
+import com.example.eneapp.databinding.ActivityElectricitySellBinding;
+import com.example.eneapp.databinding.FragmentSonotaBinding;
 import com.example.eneapp.ui.coin.result.PracticeActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class EllectricityBuyActivity extends AppCompatActivity {
+
+    private ActivityElectricitySellBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,34 @@ public class EllectricityBuyActivity extends AppCompatActivity {
         String text = intent.getStringExtra("buy2");
         TextView textView = (TextView)this.findViewById(R.id.textView16);
         textView.setText("取引電力:     "+text+"kWh");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.child("storage_battery").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                HashMap value = (HashMap) dataSnapshot.getValue();
+                Log.d(TAG, "Value is: " + value);
+
+                long souhatuden = (long) value.get("souhatuden");
+                long syouhi = (long) value.get("syouhi");
+                long yojo = (long) value.get("yojo");
+
+                TextView yojo1 = binding.yojobuy1;
+                yojo1.setText(value.get("yojo") + " kWh");
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
 
 
